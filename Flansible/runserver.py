2f3fdde031517020db54eac46d8b24108be76ab2
Tuk Bredsdorff <tukodeb@gmail.com>
2017-11-09 12:26:54 +0100

    Enable the Flask reloader

--- Flansible/runserver.py
+++ Flansible/runserver.py
@@ -9,4 +9,4 @@ if platform.node() == 'mgmt':
         pass
 
 if __name__ == '__main__':
-    app.run(debug=True, host=config.get("Default", "Flask_tcp_ip"), use_reloader=False, port=int(config.get("Default", "Flask_tcp_port")))
+    app.run(debug=True, host=config.get("Default", "Flask_tcp_ip"), use_reloader=True, port=int(config.get("Default", "Flask_tcp_port")))
