24ec39b9707d1540e2a073e8d98a21e28e65be45
malinux71 <ms@tdh-net.dk>
2020-02-26 13:30:54 +0100

    Fix loop proc.stdout.readline running forever

--- Flansible/flansible/celery_runner.py
+++ Flansible/flansible/celery_runner.py
@@ -7,7 +7,7 @@ from flansible import api, app, celery, task_timeout
 @celery.task(bind=True, soft_time_limit=task_timeout, time_limit=(task_timeout+10))
 def do_long_running_task(self, cmd, type='Ansible'):
     with app.app_context():
-        #import pudb; pudb.set_trace()
+        print(str.format("Send task: {0}", cmd))
         has_error = False
         result = None
         output = ""
@@ -18,18 +18,21 @@ def do_long_running_task(self, cmd, type='Ansible'):
         print(str.format("About to execute: {0}", cmd))
         proc = Popen([cmd], stdout=PIPE, stderr=subprocess.STDOUT, shell=True)
         for line in iter(proc.stdout.readline, ''):
-            #print(str(line))
             #print(line.decode('utf-8'))
-            #output = output + line
             output = output + line.decode('utf-8')
             self.update_state(state='PROGRESS', meta={'output': output,'description': "",'returncode': None})
+            if  proc.poll() is not None:
+                break
+
+        print(str.format("Task finished[{0}]", self.request.id))
 
         return_code = proc.poll()
         if return_code is 0:
-            meta = {'output': output, 
-                        'returncode': proc.returncode,
-                        'description': ""
-                    }
+            #meta = {'output': output, 
+            #            'returncode': proc.returncode,
+            #            'description': ""
+            #        }
+            meta = {'output': output}
             self.update_state(state='FINISHED',
                               meta=meta)
         elif return_code is not 0:
